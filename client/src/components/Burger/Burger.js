e539c19fb6631ff7aa9cbc64820e57dc8403c353
Ahmed Belhadj <mr.ahmedbelhadj@gmail.com>
2018-08-17 18:03:11 -0500

    if (dynamicIngredients.length === 0)

--- client/src/components/Burger/Burger.js
+++ client/src/components/Burger/Burger.js
@@ -4,11 +4,18 @@ import Ingredient from "./Ingredient/Ingredient";
 import classes from "./Burger.css";
 
 const Burger = props => {
-  const dynamicIngredients = Object.keys(props.ingredients).map(type => {
-    return [...Array(props.ingredients[type])].map((_, index) => {
-      return <Ingredient key={type + index} type={type} />;
-    });
-  });
+  let dynamicIngredients = Object.keys(props.ingredients)
+    .map(type => {
+      return [...Array(props.ingredients[type])].map((_, index) => {
+        return <Ingredient key={type + index} type={type} />;
+      });
+    })
+    .reduce((array, element) => {
+      return array.concat(element);
+    }, []);
+  if (dynamicIngredients.length === 0) {
+    dynamicIngredients = <p>Start adding ingredients to build your burger!</p>;
+  }
   return (
     <div className={classes.burger}>
       <Ingredient type="top-bun" />
--- client/src/containers/Builder/Builder.js
+++ client/src/containers/Builder/Builder.js
@@ -6,10 +6,10 @@ import Burger from "../../components/Burger/Burger";
 class Builder extends Component {
   state = {
     ingredients: {
-      lettuce: 1,
-      bacon: 1,
-      cheese: 2,
-      patty: 2
+      lettuce: 0,
+      bacon: 0,
+      cheese: 0,
+      patty: 0
     }
   };
   render() {
