331106294eb8f395c632ac97968759d18ebeed2d
Ahmed Belhadj <mr.ahmedbelhadj@gmail.com>
2018-08-17 17:54:03 -0500

    dynamicIngredients

--- client/src/components/Burger/Burger.js
+++ client/src/components/Burger/Burger.js
@@ -3,12 +3,16 @@ import React from "react";
 import Ingredient from "./Ingredient/Ingredient";
 import classes from "./Burger.css";
 
-const Burger = () => {
+const Burger = props => {
+  const dynamicIngredients = Object.keys(props.ingredients).map(type => {
+    return [...Array(props.ingredients[type])].map((_, index) => {
+      return <Ingredient key={type + index} type={type} />;
+    });
+  });
   return (
     <div className={classes.burger}>
       <Ingredient type="top-bun" />
-      <Ingredient type="cheese" />
-      <Ingredient type="patty" />
+      {dynamicIngredients}
       <Ingredient type="bottom-bun" />
     </div>
   );
--- client/src/containers/Builder/Builder.js
+++ client/src/containers/Builder/Builder.js
@@ -4,11 +4,18 @@ import hoc from "../../hoc/hoc";
 import Burger from "../../components/Burger/Burger";
 
 class Builder extends Component {
-  state = {};
+  state = {
+    ingredients: {
+      lettuce: 1,
+      bacon: 1,
+      cheese: 2,
+      patty: 2
+    }
+  };
   render() {
     return (
       <hoc>
-        <Burger />
+        <Burger ingredients={this.state.ingredients} />
         <div>Controls</div>
       </hoc>
     );
