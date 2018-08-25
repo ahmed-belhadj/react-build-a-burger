eb1e938b9e657e84a75df425c71de72bf470db47
Ahmed Belhadj <mr.ahmedbelhadj@gmail.com>
2018-08-25 13:28:36 -0500

    Modal shouldComponentUpdate()

--- client/src/components/Modal/Modal.js
+++ client/src/components/Modal/Modal.js
@@ -1,24 +1,29 @@
-import React from "react";
+import React, { Component } from "react";
 
 import classes from "./Modal.css";
 import hoc from "../../hoc/hoc";
 import Backdrop from "../Backdrop/Backdrop";
 
-const Modal = props => {
-  return (
-    <hoc>
-      <Backdrop show={props.show} clicked={props.closeModal} />
-      <div
-        className={classes.modal}
-        style={{
-          transform: props.show ? "translateY(0)" : "translateY(-100vh)",
-          opacity: props.show ? "1" : "0"
-        }}
-      >
-        {props.children}
-      </div>
-    </hoc>
-  );
-};
+class Modal extends Component {
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextProps.show !== this.props.show;
+  }
+  render() {
+    return (
+      <hoc>
+        <Backdrop show={this.props.show} clicked={this.props.closeModal} />
+        <div
+          className={classes.modal}
+          style={{
+            transform: this.props.show ? "translateY(0)" : "translateY(-100vh)",
+            opacity: this.props.show ? "1" : "0"
+          }}
+        >
+          {this.props.children}
+        </div>
+      </hoc>
+    );
+  }
+}
 
 export default Modal;
--- client/src/components/OrderSummary/OrderSummary.js
+++ client/src/components/OrderSummary/OrderSummary.js
@@ -1,33 +1,38 @@
-import React from "react";
+import React, { Component } from "react";
 
 import Button from "../Button/Button";
 
-const OrderSummary = props => {
-  const ingredientSummary = Object.keys(props.ingredients).map(ingredient => {
-    return (
-      <li key={ingredient}>
-        <span style={{ textTransform: "capitalize" }}>{ingredient}</span>:
-        {props.ingredients[ingredient]}
-      </li>
+class OrderSummary extends Component {
+  componentWillUpdate() {}
+  render() {
+    const ingredientSummary = Object.keys(this.props.ingredients).map(
+      ingredient => {
+        return (
+          <li key={ingredient}>
+            <span style={{ textTransform: "capitalize" }}>{ingredient}</span>:
+            {this.props.ingredients[ingredient]}
+          </li>
+        );
+      }
     );
-  });
-  return (
-    <div>
-      <h3>Order summary</h3>
-      <p>Ingredients:</p>
-      <ul>{ingredientSummary}</ul>
-      <p>
-        <strong>Total: ${props.price.toFixed(2)}</strong>
-      </p>
-      <p>Continue?</p>
-      <Button type="danger" clicked={props.cancelPurchase}>
-        CANCEL
-      </Button>
-      <Button type="success" clicked={props.continuePurchase}>
-        CONTINUE
-      </Button>
-    </div>
-  );
-};
+    return (
+      <div>
+        <h3>Order summary</h3>
+        <p>Ingredients:</p>
+        <ul>{ingredientSummary}</ul>
+        <p>
+          <strong>Total: ${this.props.price.toFixed(2)}</strong>
+        </p>
+        <p>Continue?</p>
+        <Button type="danger" clicked={this.props.cancelPurchase}>
+          CANCEL
+        </Button>
+        <Button type="success" clicked={this.props.continuePurchase}>
+          CONTINUE
+        </Button>
+      </div>
+    );
+  }
+}
 
 export default OrderSummary;
